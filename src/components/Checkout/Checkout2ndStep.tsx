06a78668fae5acefb7bbad1fe4eee3dd9f94eec5
mkksdx2 <chaipot.k@ku.th>
2023-09-06 22:57:52 +0700

    decorate button

--- src/components/Checkout/Checkout2ndStep.tsx
+++ src/components/Checkout/Checkout2ndStep.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import Image from "next/image";
+import Head from 'next/head'
+import { useState } from 'react'
+import { useRouter } from 'next/router'
 
 const Component2 = () => {
     return (
@@ -10,6 +13,14 @@ const Component2 = () => {
             <div className='font-montserrat mx-auto lg:max-w-7xl md:items-center md:flex-col md:px-8 mb-10'>
                 <div className='text-2xl font-bold'>
                     <h1>Payment</h1>
+                </div>
+                <div className='flex flex-row-2 justify-between mt-6 '>
+                    <div className='w-8/12 rounded-md bg-black'>
+                        <div className='text-white font-kanit font-bold text-xl mb-3'>1</div>
+                    </div>
+                    <div className='flex flex-col justify-evenly pl-8 w-4/12 pt-14'>
+                        <div className='text-black font-montserrat text-xl mb-3'>To protect your interests, please ensure the information is correct before proceeding with the payment.</div>
+                    </div>
                 </div>
             </div>
         </div>
--- src/pages/checkout.tsx
+++ src/pages/checkout.tsx
@@ -6,6 +6,49 @@ import { useRouter } from 'next/router'
 import Component1 from '../components/Checkout/Checkout1stStep'
 import Component2 from '../components/Checkout/Checkout2ndStep'
 import Component3 from '../components/Checkout/Checkout3rdStep'
+import Link from 'next/link'
+
+function ButtonCheckout(step:Number, handleNext:any, handleBack:any, router:any) {
+    switch (step) {
+        case 1: 
+            return <div className='flex justify-between mx-auto md:items-center md:px-8 pb-10'>
+                        <button className='border-2 border-black rounded-md text-2xl px-14 hover:bg-black hover:text-white py-2'
+                        onClick={handleBack}>
+                            Cancel
+                        </button>
+                        <button className='border-2 border-black rounded-md text-2xl text-white bg-black px-32 hover:bg-white hover:text-black py-2'
+                        onClick={handleNext}>
+                            Next
+                        </button>
+                    </div>
+        case 2:
+            return <div className='flex justify-between mx-auto md:items-center md:px-8 pb-10'>
+                        <button className='border-2 border-black rounded-md text-2xl px-14 hover:bg-black hover:text-white py-2'
+                        onClick={handleBack}>
+                            Back
+                        </button>
+                        <button className='border-2 border-black rounded-md text-2xl text-white bg-black px-32 hover:bg-white hover:text-black py-2'
+                        onClick={handleNext}>
+                            Proceed
+                        </button>
+                    </div>
+        case 3:
+            return <div className='flex justify-center mx-auto md:items-center md:px-8 pb-10'>
+                        <Link href='/main'>
+                            <button className='border-2 border-black rounded-md text-2xl text-white bg-black px-32 hover:bg-white hover:text-black py-2 mr-10'>
+                                My Ticket
+                            </button>
+                        </Link>
+                        <Link href='/main'>
+                            <button className='border-2 border-black rounded-md text-2xl px-14 hover:bg-black hover:text-white py-2'>
+                                Home
+                            </button>
+                        </Link>
+                    </div>
+        default:
+            return null;
+    }
+}
 
 const checkout = () => {
     
@@ -22,6 +65,7 @@ const checkout = () => {
     const handleBack = () => {
         setStep(step - 1)
         if (step === 1) {
+            alert('Go to Event Detail')
             return router.push('/main')
         }
     }
@@ -52,26 +96,17 @@ const checkout = () => {
             <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@400;500;700&family=Montserrat:wght@400;500;700&display=swap" rel="stylesheet"/>
         </Head>
         <Navbar/>
-        <div className='bg-[#F9F9F9]'>  
-            {/* timeline steps and Ticket Summary*/}
+        <div className='bg-[#F9F9F9] h-full'>  
             {/* step 1: Timeline 1
                         Review Ticket Information
                 step 2: Timeline 2
                         Payment
                 step 3: Timeline 3
                         Congratulations*/}
+            {/* timeline steps and Ticket Summary*/}
             {handleComponent()}
-            {/*Button Cancel and Next by San*/}
-            <div className='flex justify-between mx-auto md:items-center md:px-8 pb-10'>
-                <button className='border-2 border-black rounded-md text-2xl px-14 hover:bg-black hover:text-white'
-                onClick={handleBack}>
-                    Cancel
-                </button>
-                <button className='border-2 border-black rounded-md text-2xl text-white bg-black px-32 hover:bg-white hover:text-black'
-                onClick={handleNext}>
-                    Next
-                </button>
-            </div>
+            {/*Button Cancel and Next*/}
+            {ButtonCheckout(step, handleNext, handleBack, router)}
         </div>
     </div>
   )
