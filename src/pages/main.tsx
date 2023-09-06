b542301887fdc1a0452d81dfc1d8fc584d8f2027
chachoonnn <87368957+chachoonnn@users.noreply.github.com>
2023-09-06 21:57:34 +0700

    change font

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -6,7 +6,7 @@ import Recommendevent from '../components/events/recommendevent'
 import Allevent from '../components/events/allevent'
 import Monthlyevent from '../components/events/monthlyevent'
 import { getAllEvent } from '../service/api'
-
+import Head from "next/head";
 
 export const main = () => {
   const [events, setEvents] = useState([]);
@@ -24,6 +24,15 @@ export const main = () => {
 
   return (
     <>
+      <Head>
+        {/* import font to page */}
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='anonymous'/>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap" rel="stylesheet"/>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='anonymous'/>
+        <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@400;500;700&family=Montserrat:wght@400;500;700&display=swap" rel="stylesheet"/>
+      </Head>
         <Navbar/>
         <Introevent/>
         <div className='flex flex-col'>
