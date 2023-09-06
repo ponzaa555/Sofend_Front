9b0f9f1d4b799215baedb1a4ad2a650c47567747
kyohonda <praewa.ho@ku.th>
2023-09-06 21:29:52 +0700

    Finish api get all event

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import Navbar from '../components/navbar'
 import Footer from '../components/footer'
 import Introevent from '../components/events/introevent'
 import Recommendevent from '../components/events/recommendevent'
 import Allevent from '../components/events/allevent'
 import Monthlyevent from '../components/events/monthlyevent'
+import { getAllEvent } from '../service/api'
+
 
 export const main = () => {
+  const [events, setEvents] = useState([]);
+  
+  //fetch all event from api
+  useEffect(() => {
+    getAllEvent()
+      .then(data => {
+        setEvents(data);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  }, []);
+
   return (
     <>
         <Navbar/>
