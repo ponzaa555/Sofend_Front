54a2fca9004400095712942770c505523345b80b
kyohonda <praewa.ho@ku.th>
2023-09-14 21:26:44 +0700

    add css

--- src/components/events/eventschedule.tsx
+++ src/components/events/eventschedule.tsx
@@ -20,15 +20,19 @@ const eventschedule = () => {
     return (
         <div>
             <div>eventschedule</div>
-            <div className="grid grid-cols-4 md:grid-cols-4 sm:grid-cols-2 xs:grid-cols-1 place-content-center ">
-            {showevents.map((eventcard, index) => (
-                <a key = {index} className="flex flex-col items-end">
-                    <div className="flex">
-                        <ScheduleCard image={eventcard.posterImage} datestart={eventcard.startDateTime} dateend={eventcard.endDateTime} name={eventcard.eventName} place={eventcard.location} />
-                    </div>
-                    <button className="flex w-full h-14 bg-black text-white font-bold rounded-xl px-4 py-2 ">SCAN</button>
-                </a>
-            ))}
+            <div className="mt-8 mr-6 ml-6">
+                <div className="grid grid-cols-4 md:grid-cols-4 sm:grid-cols-2 xs:grid-cols-1 place-content-center ">
+                {showevents.map((eventcard, index) => (
+                    <a key = {index} className="flex flex-col items-center">
+                        <div className="flex-1">
+                            <ScheduleCard image={eventcard.posterImage} datestart={eventcard.startDateTime} dateend={eventcard.endDateTime} name={eventcard.eventName} place={eventcard.location} />
+                        </div>
+                        <a href = {"/scan/"+eventcard.eventID} className='w-44 h-10 mb-8 '>
+                            <button className="font-montserrat w-full h-full text-xl bg-black text-white font-semibold rounded-xl align-middle">SCAN</button>
+                        </a>
+                    </a>
+                ))}
+                </div>
             </div>
         </div>
     )
--- src/components/events/schedulecard.tsx
+++ src/components/events/schedulecard.tsx
@@ -23,44 +23,38 @@ const ScheduleCard: React.FC<SheduleitemProps> = ({ datestart,dateend,name,place
     const checkdate = () => {
       if(daystart === dayend && monthstart === monthend && yearstart === yearend){
         return(
-          <div className="text-[#D40000] text-base font-bold mt-4">{parseInt(daystart)} {month[parseInt(monthstart)-1]} {parseInt(yearstart)}</div>
+          <div className="font-montserrat text-[#D40000] text-sm font-bold mt-4">{parseInt(daystart)} {month[parseInt(monthstart)-1]} {parseInt(yearstart)}</div>
         )
       }else if(monthstart === monthend && yearstart === yearend && daystart !== dayend){
         return(
-          <div className="text-[#D40000] text-base font-bold mt-4">{parseInt(daystart)} - {parseInt(dayend)} {month[parseInt(monthstart)-1]} {parseInt(yearstart)}</div>
+          <div className="font-montserrat text-[#D40000] text-sm font-bold mt-4">{parseInt(daystart)} - {parseInt(dayend)} {month[parseInt(monthstart)-1]} {parseInt(yearstart)}</div>
         )
       }else if(yearstart === yearend && monthstart !== monthend){
         return(
-          <div className="text-[#D40000] text-base font-bold mt-4">{parseInt(daystart)} {month[parseInt(monthstart)-1]} - {parseInt(dayend)} {month[parseInt(monthend)-1]} {parseInt(yearstart)}</div>
+          <div className="font-montserrat text-[#D40000] text-sm font-bold mt-4">{parseInt(daystart)} {month[parseInt(monthstart)-1]} - {parseInt(dayend)} {month[parseInt(monthend)-1]} {parseInt(yearstart)}</div>
         )
       }else{
         return(
-          <div className="text-[#D40000] text-base font-bold mt-4">{parseInt(daystart)} {month[parseInt(monthstart)-1]} {parseInt(yearstart)} - {parseInt(dayend)} {month[parseInt(monthend)-1]} {parseInt(yearend)}</div>
+          <div className="font-montserrat text-[#D40000] text-sm font-bold mt-4">{parseInt(daystart)} {month[parseInt(monthstart)-1]} {parseInt(yearstart)} - {parseInt(dayend)} {month[parseInt(monthend)-1]} {parseInt(yearend)}</div>
         )
       }
     }
 
-    const nameshow1 = name.slice(0,18)
-    const nameshow2 = name.slice(18,36)
-    const nameshow3 = name.slice(36)
-    const placeshow1 = place.slice(0,18)
-    const placeshow2 = place.slice(18,36)
-    const placeshow3 = place.slice(36)
+    const showname1 = name.slice(0,16)
+    const showname2 = name.slice(16,32)
+    const showname3 = name.slice(32)
     
     return (
-      <div className="w-52 rounded overflow-hidden">
-          <img src={image} alt="event" className=" w-full h-80 rounded-xl shadow-xl"/>
+      <div className="w-44 rounded overflow-hidden">
+          <img src={image} alt="event" className=" w-full h-60 rounded-xl shadow-xl mb-1"/>
           <div className="w-full">
             {checkdate()}
             <div>
-                <h2 className="text-[#000000]  text-lg font-bold mt-2 mb-2 ">{nameshow1}</h2>
-                <h2 className="text-[#000000]  text-lg font-bold mb-2 ">{nameshow2}</h2>
-                <h2 className="truncate h-16 text-[#000000] break-normal  text-lg font-bold mb-2 ">{nameshow3}</h2>
+                <h2 className="font-montserrat text-[#000000]  text-base font-bold ">{showname1}</h2>
+                <h2 className="font-montserrat text-[#000000]  text-base font-bold ">{showname2}</h2>
+                <h2 className="font-montserrat text-[#000000]  text-base font-bold truncate mb-2">{showname3}</h2>
             </div>
-            <p className="text-[#A7A7A7]">{placeshow1}</p>
-            <p className="text-[#A7A7A7]">{placeshow2}</p>
-            <p className="truncate h-16 text-[#A7A7A7]">{placeshow3}</p>
-
+            <p className="font-montserrat text-[#A7A7A7] text-xs mb-4">{place}</p>
           </div>
       </div>
     );
