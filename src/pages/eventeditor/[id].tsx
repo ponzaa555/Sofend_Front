4b836389b9b7a2c5769662544dd255d210ece764
FernPK <pornnapat5563@gmail.com>
2023-09-25 22:51:52 +0700

    add Head (import font)

--- src/pages/eventeditor/[id].tsx
+++ src/pages/eventeditor/[id].tsx
@@ -1,8 +1,20 @@
+import Head from 'next/head'
 import React from 'react'
 
 const EventEditor = () => {
   return (
-    <div>EventEditor</div>
+    <div>
+      <Head>
+        {/* import font to page */}
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='anonymous'/>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap" rel="stylesheet"/>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='anonymous' />
+        <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@400;500;700&family=Montserrat:wght@400;500;700&display=swap" rel="stylesheet"/>
+      </Head>
+      {/* <NavbarEO /> */}
+    </div>
   )
 }
 
