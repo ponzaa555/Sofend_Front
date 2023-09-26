a18ba306554f0347092348ff1966a73fe2b987e3
FernPK <pornnapat5563@gmail.com>
2023-09-26 10:31:38 +0700

    default layout

--- src/pages/eventeditor/[id].tsx
+++ src/pages/eventeditor/[id].tsx
@@ -1,8 +1,36 @@
 import Head from 'next/head'
-import React from 'react'
+import React, { useEffect } from 'react'
+import MenubarEO from '~/components/menubarEO';
 import NavbarEO from '~/components/navbarEO'
+import ContentDashboard from '../../components/eventEditorContent/contentDashboard';
+import ContentEventSetting from '../../components/eventEditorContent/contentEventSetting';
+import ContentTicketTypes from '../../components/eventEditorContent/ContentTicketTypes';
+import ContentStaff from '../../components/eventEditorContent/ContentStaff';
+import ContentPayment from '../../components/eventEditorContent/contentPayment';
 
 const EventEditor = () => {
+
+  const [contentId, setContentId] = React.useState("menubar-dashboard")
+  const [content, setContent] = React.useState(<ContentDashboard />)
+
+  useEffect(() => {
+    if(contentId === 'menubar-dashboard') {
+      setContent(<ContentDashboard />)
+    }
+    else if(contentId === 'menubar-event-setting') {
+      setContent(<ContentEventSetting />)
+    }
+    else if(contentId === 'menubar-ticket-types') {
+      setContent(<ContentTicketTypes />)
+    }
+    else if(contentId === 'menubar-staff') {
+      setContent(<ContentStaff />)
+    }
+    else if(contentId === 'menubar-payment') {
+      setContent(<ContentPayment />)
+    }
+  }, [contentId])
+
   return (
     <div>
       <Head>
@@ -15,6 +43,10 @@ const EventEditor = () => {
         <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@400;500;700&family=Montserrat:wght@400;500;700&display=swap" rel="stylesheet"/>
       </Head>
       <NavbarEO />
+      <MenubarEO setContentId={setContentId}/>
+      <div className='mx-auto lg:max-w-7xl md:items-center md:flex-col md:px-8 mb-10 font-montserrat'>
+        {content}
+      </div>
     </div>
   )
 }
