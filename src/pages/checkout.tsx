5faad723d7bd3d3f12a7bdf041eb17b427de8042
mkksdx2 <chaipot.k@ku.th>
2023-09-26 14:42:22 +0700

    use localstrage

--- src/pages/checkout.tsx
+++ src/pages/checkout.tsx
@@ -8,7 +8,6 @@ import Checkout1stStep from '../components/Checkout/Checkout1stStep'
 import Checkout2ndStep from '../components/Checkout/Checkout2ndStep'
 import Checkout3rdStep from '../components/Checkout/Checkout3rdStep'
 import Link from 'next/link'
-import { type } from "os";
 
 function ButtonCheckout(step:Number, handleNext:any, handleBack:any) {
     switch (step) {
@@ -68,6 +67,8 @@ const Checkout: React.FC = ({}) => {
     const [step, setStep] = useState(1)
     const {data:session} = useSession();
     const [eventData, setEventData] = useState<EventData>()
+    const [loading, setLoading] = useState(false)
+    
  
     // function to handle next button.
     const handleNext = () => {
@@ -93,18 +94,14 @@ const Checkout: React.FC = ({}) => {
         });
     }
     
-    // const eventData = JSON.parse(localStorage.getItem('data')!)
     useEffect(() => {
+        setLoading(false)
         setEventData(JSON.parse(localStorage.getItem('data')!))
-        console.log(JSON.parse(localStorage.getItem('data')!).eventName)
+        setLoading(true)
     }, [])
 
     // function to handle component by step and pass data to component.
     const handleComponent = (session:any) => {
-        // console.log(localStorage === window.localStorage)
-        // const data = localStorage.getItem('data')
-        // const eventData = JSON.parse(data!)
-        console.log(eventData)
 
         switch (step) {
             case 1:
@@ -154,6 +151,7 @@ const Checkout: React.FC = ({}) => {
                 <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@400;500;700&family=Montserrat:wght@400;500;700&display=swap" rel="stylesheet"/>
             </Head>
             <Navbar/>
+            { loading == true ?
             <div className='bg-[#F9F9F9] h-full min-h-full pb-10'>  
                 {/* step 1: Timeline 1
                             Review Ticket Information
@@ -168,7 +166,15 @@ const Checkout: React.FC = ({}) => {
                 
                 {/*Button Cancel and Next*/}
                 {ButtonCheckout(step, handleNext, handleBack)}
+            </div>:
+            <div role="status" className='flex flex-row items-center justify-center mb-5 mt-4 '>
+                <svg aria-hidden="true" className="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+                    <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+                </svg>
+                <span className="font-montserrat">Loading...</span>
             </div>
+            }
         </div>
     )
 }
