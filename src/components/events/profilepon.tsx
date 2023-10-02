c543bab9cf6adbbef7ad3f6d2bb2ac797873de4f
mkksdx2 <chaipot.k@ku.th>
2023-10-02 23:17:08 +0700

    add loading

--- src/components/events/profilepon.tsx
+++ src/components/events/profilepon.tsx
@@ -12,12 +12,6 @@ import EventSchedule from '~/components/events/eventschedule';
 import { Session } from 'inspector';
 import { useRouter } from "next/router";
 
-
-
-// ใชั usesession แล้ว undefind คณะ usestae ทำให้ Editprofile ไม่ได้ !!!!!!!!
-// e.preventDefualt() ใช้ยังไง
-
-
 interface edit_profile {
     userID: string;
     newEmail: string;
@@ -31,11 +25,7 @@ interface changepass {
     newPassword: string;
 }
 
-
-
 const profilepage = (props: any) => {
-
-
     const Event = props
     const router = useRouter()
     const { data: session } = useSession()
@@ -217,17 +207,27 @@ const profilepage = (props: any) => {
             <Toaster />
             <div className=' flex justify-center gap-10 md:text-base lg:text-xl sm:text-sm w-full'>
                 <div className='  w-[18%] bg-white    h-1/2  shadow-lg rounded-2xl '>
-                    <div className=' grid justify-items-center h-1/2 bg-white rounded-2xl w-full grid-cols-1'>
-                        <div className=' h-5'></div>
-                        <img src={"images/events/user.png"} className=' h-16 justify-center ' />
-                        { getSessionFinish == true ?
-                        <p className='font-montserrat text-center mt-1 text-black font-bold md:text-base sm:text-sm'>{data1.firstName} {data1.lastName}</p>
-                        <h2 className='font-montserrat text-center text-gray-400 md:text-base sm:text-sm '>{data1.email}</h2>
-                        : 
-                    }
-                        <div className='h-1 w-4/5 bg-slate-200 mt-2 rounded-2xl  '></div>
-                    
-                    </div>
+                    { getSessionFinish == true?
+                        <div className=' grid justify-items-center h-1/2 bg-white rounded-2xl w-full grid-cols-1'>
+                            <div className=' h-5'></div>
+                            <img src={"images/events/user.png"} className=' h-16 justify-center ' />
+                            <p className='font-montserrat text-center mt-1 text-black font-bold md:text-base sm:text-sm'>{data1.firstName} {data1.lastName}</p>
+                            <h2 className='font-montserrat text-center text-gray-400 md:text-base sm:text-sm '>{data1.email}</h2>
+                            <div className='h-1 w-4/5 bg-slate-200 mt-2 rounded-2xl  '></div>
+                        </div> : 
+                        <div className=' grid justify-items-center h-1/2 bg-white rounded-2xl w-full grid-cols-1'>
+                            <div className=' h-5'></div>
+                            <img src={"images/events/user.png"} className=' h-16 justify-center ' />
+                            <div role="status" className='flex flex-row items-center justify-center mb-5 mt-4 '>
+                                <svg aria-hidden="true" className="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+                                    <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
+                                </svg>
+                                <span className="font-montserrat">Loading...</span>
+                            </div>                            
+                            <div className='h-1 w-4/5 bg-slate-200 mt-2 rounded-2xl  '></div>
+                        </div>
+                    }   
                     <div className=' h-1/2  rounded-2xl w-full'>
                         <button className=' flex h-1/3 bg-white hover:bg-slate-50 shadow-2xl cursor-pointer w-full ' value="show1" onClick={() => { setshowfunc("show1") }}  >
                             {
