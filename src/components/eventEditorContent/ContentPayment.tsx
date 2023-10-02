5e53a1a9cf36e642538b591929121383e50bb586
ponza555 <ponpon13173@gmail.com>
2023-10-02 17:49:47 +0700

    fix front

--- src/components/eventEditorContent/ContentPayment.tsx
+++ src/components/eventEditorContent/ContentPayment.tsx
@@ -11,18 +11,18 @@ const ContentPayment = () => {
             Bank Accounts
           </div>
           <div>
-            <div className='text-base pt-2'>
+            <div className='text-xl pt-2'>
               <div className='flex'>
                 <h1>Bank</h1>
                 <h1 className=' text-red-600 pl-1'>*</h1>
               </div>
               <div>
-                <input type="text" className='w-full rounded-md h-8  border-gray-600 border-[1.5px] bg-transparent ' />
+                <input type="text" className='w-full rounded h-9  border-gray-500 border bg-transparent ' />
               </div>
             </div>
           </div>
           <div id='Nameandtype' className='pt-2'>
-            <div className='flex text-base gap-4'>
+            <div className='flex text-xl gap-4'>
               <div className='w-3/5 flex'>
                 <h1>Account Name</h1>
                 <h1 className='pl-1 text-red-600'>*</h1>
@@ -34,18 +34,18 @@ const ContentPayment = () => {
             </div>
               <div id='inputNametype' className='flex gap-4'>
                 <div className='w-3/5'>
-                  <input className='w-full rounded-md h-8  border-gray-600 border-[1.5px] bg-transparent ' />
+                  <input className='w-full rounded h-9  border-gray-500 border bg-transparent ' />
                 </div>
                 <div className='w-2/5'>
-                  <input className='w-full rounded-md h-8  border-gray-600 border-[1.5px] bg-transparent ' />
+                  <input className='w-full rounded h-9  border-gray-500 border bg-transparent' />
                 </div>
               </div>
             <div>
             </div>
           </div>
           <div id='accountNo.Brach' className='pt-2'>
-            <div className='text-base'>
-              <div className='flex text-base gap-4'>
+            <div className='text-xl'>
+              <div className='flex text-xl gap-4'>
                 <div className='flex w-3/5'>
                   <h1>Account No.</h1>
                   <h1 className='pl text-red-600'>*</h1>
@@ -58,10 +58,10 @@ const ContentPayment = () => {
             </div>
             <div id='inputAccountNoandBranch' className='flex gap-4'>
               <div className='w-3/5'>
-                <input className='w-full rounded-md h-8  border-gray-600 border-[1.5px] bg-transparent ' />
+                <input className='w-full rounded h-9  border-gray-500 border bg-transparent' />
               </div>
               <div className='w-2/5'>
-                <input className='w-full rounded-md h-8  border-gray-600 border-[1.5px] bg-transparent ' />
+                <input className='w-full rounded h-9  border-gray-500 border bg-transparent ' />
               </div>
             </div>
           </div>
@@ -70,13 +70,13 @@ const ContentPayment = () => {
           </div>
         </form>
        
-        <div id='right' className=' w-1/3 h-full '>
+        <div id='right' className=' w-1/3 h-full'>
           <div className='h-2'></div>
           <div className='pt-2'>
             <div>
-              <h1 className=' font-semibold text-base '>Bank Accounts</h1>
+              <h1 className=' font-semibold text-lg'>Bank Accounts</h1>
             </div>
-            <div className='text-xs '>
+            <div className='text-base '>
               <h1 className='flex'>Bank Account will need to get verified</h1>
               <h1 className='flex'>and approved before used on following</h1>
               <h1 className='flex'>purpose.</h1>
