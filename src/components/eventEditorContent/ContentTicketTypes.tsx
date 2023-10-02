ec720d278f45d6f00cb7af38c25a393c98e681d3
chachoonnn <87368957+chachoonnn@users.noreply.github.com>
2023-10-03 01:42:45 +0700

    can click create new ticket type button

--- src/components/eventEditorContent/ContentTicketTypes.tsx
+++ src/components/eventEditorContent/ContentTicketTypes.tsx
@@ -1,37 +1,50 @@
-import React from 'react'
-import CreateNewTicketTypes from './ContentCreateNewTicketTypes'
-
-// type Props = {}
+import React, { useState } from 'react';
+import ContentCreateNewTicketTypes from './ContentCreateNewTicketTypes';
 
 const ContentTicketTypes = () => {
+  const [isCreatingNewTicketType, setIsCreatingNewTicketType] = useState(false);
+
+  const handleCreateNewTicketTypeClick = () => {
+    setIsCreatingNewTicketType(true);
+  };
+
+  const handleCancelCreateNewTicketType = () => {
+    setIsCreatingNewTicketType(false);
+  };
+
   return (
     <>
-      <div className="divide-y-2">
-        <div className="flex justify-between my-5">
-          <div className="font-bold text-4xl">Ticket Types</div>
-          <button className="bg-black text-white font-bold text-base py-2 px-4 rounded-lg">
-            create new ticket type
-          </button>
+      {isCreatingNewTicketType ? (
+        <ContentCreateNewTicketTypes onCancel={handleCancelCreateNewTicketType} />
+      ) : (
+        <div className="divide-y-2">
+          <div className="flex justify-between my-5">
+            <div className="font-bold text-4xl">Ticket Types</div>
+            <button
+              className="bg-black text-white border-2 border-black hover:bg-white hover:text-black font-bold text-base py-2 px-4 rounded-lg "
+              onClick={handleCreateNewTicketTypeClick}
+            >
+              create new ticket type
+            </button>
+          </div>
+          <div>
+            <ul className="flex flex-row justify-between my-5 mx-20 font-bold text-xl">
+              <li>Name</li>
+              <li></li>
+              <li>Price</li>
+              <li>Sold</li>
+              <li>Quota</li>
+            </ul>
+          </div>
+          <div></div>
         </div>
-        <div>
-          <ul className="flex flex-row justify-between my-5 mx-20 font-bold text-xl">
-            <li>Name</li>
-            <li></li>
-            <li>Price</li>
-            <li>Sold</li>
-            <li>Quota</li>
-          </ul>
-        </div>
-        <div>
-
-        </div>
-      </div>
+      )}
       <div className="flex flex-grow flex-col text-center my-5">
         <div className="font-bold text-4xl">EventBud</div>
-        <div className="">all right reserve</div>
+        <div className="">all right reserved</div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default ContentTicketTypes
+export default ContentTicketTypes;
