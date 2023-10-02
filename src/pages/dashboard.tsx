360bb41665bbef31eedb85ebfe45f2d821dbba9c
qxntm <russarin.n@ku.th>
2023-10-02 14:20:37 +0700

    connect API wait for login page

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -9,14 +9,21 @@ const dashboard = ({}) => {
   const [event, setEvent] = useState([]);
 
   useEffect(() => {
-    axios.get('https://eventbud-jujiu2awda-uc.a.run.app/eo_event/{organizerID}')
+    axios.get('https://eventbud-jujiu2awda-uc.a.run.app/eo_event/rajadamnern')
       .then((response) => {
         setEvent(response.data);
+        console.log(response.data);
       })
       .catch((error) => {
         console.error('Error fetching data:', error);
       });
   }, []);
+
+  useEffect(() => {
+    console.log("event: ",event)
+  },[event])
+
+  
 
   return (
     <>
@@ -37,7 +44,7 @@ const dashboard = ({}) => {
           <button className='font-montserrat font-bold bg-black rounded-lg text-white px-3'>create an event</button>
         </div>
         {/*table header*/}
-        <div className='w-full h-1 bg-gray-300 rounded-lg mb-5' />
+        <div className='w-full h-0.5 bg-gray-300 rounded-lg mb-5' />
         <div className='flex font-montserrat text-xl font-bold mb-5 text-2xl'>
           <div className='pl-5 w-6/12'>Event Name</div>
           <div className='w-2/12'>Date & Time</div>
@@ -46,12 +53,26 @@ const dashboard = ({}) => {
           <div className='w-2/12'>Tickets Sold</div>
           <div className='w-1/12'></div>
         </div>
-        <div className='w-full h-1 bg-gray-300 rounded-lg mb-5' />
-        <Event />
-        <Event />
-        <Event />
+        <div className='w-full h-0.5 bg-gray-300 rounded-lg mb-5' />
+        {event.map((event) => (
+          <Event
+            eventName={event.eventName}
+            startDateTime={event.startDateTime}
+            endDateTime={event.endDateTime}
+            eventStatus={event.eventStatus}
+            ticketSold={event.ticketSold}
+            posterImage={event.posterImage}
+            soldTicket={event.soldTicket}
+            totalTicket={event.totalTicket}
+          />
+          ))}
+        {/* <Event /> */}
 
-        
+        <div className='justify-center bg-white p-4'>
+          <h1 className='text-center text-black font-montserrat font-bold text-2xl'>EventBud</h1>
+          <h1 className='text-center text-black font-montserrat'>all right reserved</h1>
+        </div>
+
       </div>
     </>
   )
