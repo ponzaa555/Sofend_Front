93008a2918dc17e793b3113cccf985305faa1f96
qxntm <russarin.n@ku.th>
2023-10-04 01:15:22 +0700

    connect all API

--- src/components/eventEditorContent/ContentDashboard.tsx
+++ src/components/eventEditorContent/ContentDashboard.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import axios from 'axios';
 import { get } from 'http';
+import { type } from 'os';
 
 // type Props = {}
 
@@ -13,11 +14,35 @@ const ContentOverview = () => {
     ticketLeft: string
   }
 
+  type Ticket = {
+    eventID: string;
+    eventName: string;
+    zoneRevenue: zoneRevenue[];
+}
+
+  type ForTicketType = {
+    name: string;
+    price: number;
+    sold: number;
+    quota: number;
+    revenue: number;
+  }
 
   const [event, setEvent] = useState<Event>({ totalRevenue: '', ticketSold: '', ticketLeft: '' });
   const [getEventFinish, setgetEventFinish] = useState(false);
-  const [ticket, setTicket] = useState([]);
-  const [getTicketfinish, setGetTicketfinish] = useState(false);
+  const [ticket, setTicket] = useState<Ticket>({
+    eventID: "",
+    eventName: "",
+    zoneRevenue: []
+})
+  const [getTicketFinish, setgetTicketFinish] = useState(false);
+  const [listTicket,setlistTicket] = useState<ForTicketType[]>([{
+    name: "",
+    price: 0,
+    sold: 0,
+    quota: 0,
+    revenue: 0,
+}]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -28,10 +53,30 @@ const ContentOverview = () => {
       setgetEventFinish(true)
     }
     fetchData();
-  })
+  },[])
 
   const router = useRouter();
   const { id } = router.query as { id: string };
+
+  useEffect(() => {
+    if (id) {
+      axios.get<Ticket>('https://eventbud-jujiu2awda-uc.a.run.app/event/0189a3dfcc')
+        .then((res) => {
+          setTicket(res.data);
+          setgetTicketFinish(true)
+          setlistTicket(res.data.zoneRevenue.map(item => ({
+            name: item.className,
+            price : item.price,
+            sold : item.ticketSold,
+            quota : item.quota,
+            revenue : item.ticketSold * item.price
+          })))
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    }
+  }, [id]);
 
   return (
     <>
@@ -86,20 +131,22 @@ const ContentOverview = () => {
       <div className='w-full h-0.5 bg-gray-300 rounded-lg mb-5' />
 
       {/*Ticket Sales Table Content*/}
-      {getEventFinish ?
+      {getTicketFinish ? listTicket.map((item,index) => (
+        <div key={index}>
         <div className='flex font-montserrat text-base mb-3 text-2xl'>
-          <div className='pl-5 w-4/12'>Name</div>
-          <div className='w-2/12'>Price</div>
-          <div className='w-2/12'>Sold</div>
-          <div className='w-2/12'>Quota</div>
-          <div className='w-2/12'>Revenue</div>
-        </div> :
+          <div className='pl-5 w-4/12'>{item.name}</div>
+          <div className='w-2/12'>{item.price}</div>
+          <div className='w-2/12'>{item.sold}</div>
+          <div className='w-2/12'>{item.quota}</div>
+          <div className='w-2/12'>{item.revenue}</div>
+        </div>
+        </div>)) :
         <div role="status" className='flex flex-row items-center justify-center h-10'>
           <svg aria-hidden="true" className="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
             <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
           </svg>
-          <span className="font-montserrat font-bold">Loading...</span>
+          <span className="font-montserrat">Loading...</span>
         </div>}
 
       {/*Footer*/}
