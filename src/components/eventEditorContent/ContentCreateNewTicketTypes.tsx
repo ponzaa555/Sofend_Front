e157f7cf9f4621bc24e0f2a06194ba3a79af985f
mkksdx2 <chaipot.k@ku.th>
2023-10-04 14:02:22 +0700

    add condition 0=zone 1=seat

--- src/components/eventEditorContent/ContentCreateNewTicketTypes.tsx
+++ src/components/eventEditorContent/ContentCreateNewTicketTypes.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react'
-import { useRouter } from 'next/router'
 import ContentTicketTypes  from './ContentTicketTypes'
 interface jsonCreateNewTicketTypes {
     Name : string;
     QuantityAvailable : string;
+    NumberOfRows : string;
+    NumberOfCols : string;
     StartDate : string;
     StartTime : string;
     EndDate : string;
@@ -12,26 +13,46 @@ interface jsonCreateNewTicketTypes {
 }
 
 const ContentCreateNewTicketTypes = () => {
-    const router = useRouter()
     const [handleCancelButton, sethandleCancelButton] = useState(false)
+    const [handleTicketType, sethandleTicketType] = useState(0)
 
     const handleButton = () => {
         sethandleCancelButton(true)
     }
 
+    // 0 = zone, 1 = seat
+    // if (tickettype == seat) {
+    //     sethandleTicketType(1)
+    // }
+
     const handleOnChange = (e:React.FocusEvent) => {
         e.preventDefault()
+        // const organizerID = ...
+        // const eventID = ...
         const Name = document.getElementById('tt-name').value
-        const QuantityAvailable = document.getElementById('tt-qa').value
+        let QuantityAvailable = ''
+        let NumberOfRows = ''
+        let NumberOfCols = ''
         const StartDate = document.getElementById('tt-start-date').value
         const StartTime = document.getElementById('tt-start-time').value
         const EndDate = document.getElementById('tt-end-date').value
         const EndTime = document.getElementById('tt-end-time').value
         const TicketPrice = document.getElementById('tt-ticketprice').value
         
+        // 0 = zone, 1 = seat
+        if (handleTicketType == 0) {
+            QuantityAvailable = document.getElementById('tt-qa').value
+        }
+        else {
+            NumberOfRows = document.getElementById('tt-num-row').value
+            NumberOfCols = document.getElementById('tt-num-col').value
+        }
+        
         const testoutput:jsonCreateNewTicketTypes = {
             Name : Name,
             QuantityAvailable : QuantityAvailable,
+            NumberOfRows : NumberOfRows,
+            NumberOfCols : NumberOfCols,
             StartDate : StartDate,
             StartTime : StartTime,
             EndDate : EndDate,
@@ -54,15 +75,35 @@ const ContentCreateNewTicketTypes = () => {
                                 <div className='flex flex-col justify-start w-full'> 
                                     <label htmlFor="tt-name" className="text-xl mb-1 after:content-['*'] after:ml-0.5 after:text-red-500">Name</label>
                                     <input type="text" id='tt-name' className='border border-gray-500 rounded h-9 mb-3 font-montserrat px-2 ' required/>
-                                    <label htmlFor="tt-qa" className="text-xl mb-1 after:content-['*'] after:ml-0.5 after:text-red-500">Quantity Available</label>
-                                    <input type="text" id='tt-qa' className='border border-gray-500 rounded h-9 mb-3 font-montserrat px-2' required/>
+                                    { handleTicketType == 0 ?
+                                        <>
+                                            <label htmlFor="tt-qa" className="text-xl mb-1 after:content-['*'] after:ml-0.5 after:text-red-500">Quantity Available</label>
+                                            <input type="number" id='tt-qa' className='border border-gray-500 rounded h-9 mb-3 font-montserrat px-2' required/>
+                                        </>:
+                                        <div className='flex flex-row justify-start gap-7'>
+                                            <div className='flex flex-col justify-start w-1/2'>
+                                                <label htmlFor="tt-num-row" className="text-xl mb-1 after:content-['*'] after:ml-0.5 after:text-red-500">Number of rows</label>
+                                                <input type="number" id='tt-num-row' className='border border-gray-500 rounded h-9 mb-3 font-montserrat px-2' required/>
+                                            </div>
+                                            <div className='flex flex-col justify-start w-1/2'>
+                                                <label htmlFor="tt-num-col" className="text-xl mb-1 after:content-['*'] after:ml-0.5 after:text-red-500">Number of columns</label>
+                                                <input type="number" id='tt-num-col' className='border border-gray-500 rounded h-9 mb-3 font-montserrat px-2' required/>
+                                            </div>
+                                        </div>  
+                                    }
                                 </div>
                             </div>
                             <div className='flex flex-col w-1/3 justify-start'>
+                            { handleTicketType == 0 ? 
                                 <div>
                                     <h3 className='font-bold text-lg mb-2'>Quantity Available</h3>
                                     <p className='mb-4'>Number of available tickets for sale of this ticket type.</p>
+                                </div>:
+                                <div>
+                                    <h3 className='font-bold text-lg mb-2'>Number of rows & columns</h3>
+                                    <p className='mb-4'>Number of rows and column available in this ticket type.</p>
                                 </div>
+                            }
                             </div>
                         </div>
                         
@@ -71,24 +112,24 @@ const ContentCreateNewTicketTypes = () => {
                         <div className='flex flex-row gap-10'>
                             <div className='flex flex-col justify-start w-2/3'>
                                 <div className='flex flex-row justify-start gap-7'>
-                                <div className='flex flex-col justify-start w-7/12'>
-                                    <label htmlFor="tt-start-date" className="text-xl mb-1 after:content-['*'] after:ml-0.5 after:text-red-500">Start at date</label>
-                                    <input type="date" id='tt-start-date' className='border border-gray-500 rounded h-9 mb-3 font-montserrat px-2' required/>
-                                </div>
-                                <div className='flex flex-col justify-start w-5/12'>
-                                    <label htmlFor="tt-start-time" className="text-xl mb-1 after:content-['*'] after:ml-0.5 after:text-red-500">Start time</label>
-                                    <input type="time" id='tt-start-time' className='border border-gray-500 rounded h-9 mb-3 font-montserrat px-2' required/>
-                                </div>
+                                    <div className='flex flex-col justify-start w-7/12'>
+                                        <label htmlFor="tt-start-date" className="text-xl mb-1 after:content-['*'] after:ml-0.5 after:text-red-500">Start at date</label>
+                                        <input type="date" id='tt-start-date' className='border border-gray-500 rounded h-9 mb-3 font-montserrat px-2' required/>
+                                    </div>
+                                    <div className='flex flex-col justify-start w-5/12'>
+                                        <label htmlFor="tt-start-time" className="text-xl mb-1 after:content-['*'] after:ml-0.5 after:text-red-500">Start time</label>
+                                        <input type="time" id='tt-start-time' className='border border-gray-500 rounded h-9 mb-3 font-montserrat px-2' required/>
+                                    </div>
                                 </div>
                                 <div className='flex flex-row justify-start gap-7'>
-                                <div className='flex flex-col justify-start w-7/12'>
-                                    <label htmlFor="tt-end-date" className="text-xl mb-1 after:content-['*'] after:ml-0.5 after:text-red-500">End at Date</label>
-                                    <input type="date" id='tt-end-date' className='border border-gray-500 rounded h-9 mb-3 font-montserrat px-2' required/>
-                                </div>
-                                <div className='flex flex-col justify-start w-5/12'>
-                                    <label htmlFor="tt-end-time" className="text-xl mb-1 after:content-['*'] after:ml-0.5 after:text-red-500">End Time</label>
-                                    <input type="time" id='tt-end-time' className='border border-gray-500 rounded h-9 mb-3 font-montserrat px-2' required/>
-                                </div>
+                                    <div className='flex flex-col justify-start w-7/12'>
+                                        <label htmlFor="tt-end-date" className="text-xl mb-1 after:content-['*'] after:ml-0.5 after:text-red-500">End at Date</label>
+                                        <input type="date" id='tt-end-date' className='border border-gray-500 rounded h-9 mb-3 font-montserrat px-2' required/>
+                                    </div>
+                                    <div className='flex flex-col justify-start w-5/12'>
+                                        <label htmlFor="tt-end-time" className="text-xl mb-1 after:content-['*'] after:ml-0.5 after:text-red-500">End Time</label>
+                                        <input type="time" id='tt-end-time' className='border border-gray-500 rounded h-9 mb-3 font-montserrat px-2' required/>
+                                    </div>
                                 </div>
                             </div>
                             <div className='flex flex-col w-1/3 justify-start'>
@@ -105,7 +146,7 @@ const ContentCreateNewTicketTypes = () => {
                             <div className='w-2/3 flex flex-row gap-9'>
                                 <div className='flex flex-col justify-start w-full'>
                                     <label htmlFor="tt-ticketprice" className="text-xl mb-1 after:content-['*'] after:ml-0.5 after:text-red-500">Ticket Price (Baht)</label>
-                                    <input type="text" id='tt-ticketprice' className='border border-gray-500 rounded h-9 mb-3 font-montserrat px-2 ' required/>
+                                    <input type="number" id='tt-ticketprice' className='border border-gray-500 rounded h-9 mb-3 font-montserrat px-2 ' required/>
                                 </div>
                             </div>
                             <div className='flex flex-col w-1/3 justify-start'>
