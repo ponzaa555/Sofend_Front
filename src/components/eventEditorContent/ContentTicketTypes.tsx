e20a3162dcb5f5dc52e9ed19d7b772256684c1db
chachoonnn <87368957+chachoonnn@users.noreply.github.com>
2023-10-06 19:45:17 +0700

    change css in ticketTypes

--- src/components/eventEditorContent/ContentTicketTypes.tsx
+++ src/components/eventEditorContent/ContentTicketTypes.tsx
@@ -35,42 +35,40 @@ const ContentTicketTypes = () => {
                 <>
                     { handleCreateNewTicketType == false ?
                         <>
-                            <div className="divide-y-2">
-                                <div className="flex justify-between my-5">
-                                    <div className="font-bold text-4xl">Ticket Types</div>
+                                  <div className="flex justify-between my-5">
+                                    <div className='text-3xl font-bold'>Ticket Types</div>
                                     <button className="bg-black text-white border-2 border-black hover:bg-white hover:text-black font-bold text-base py-2 px-4 rounded-lg"
                                         onClick={handleButton}>
                                         create new ticket type
                                     </button>
-                                </div>
-                                <div>
-                                    <ul className="flex flex-row justify-between my-5 mx-20 font-bold text-xl">
-                                        <li>Name</li>
-                                        <li></li>
-                                        <li>Price</li>
-                                        <li>Sold</li>
-                                        <li>Quota</li>
-                                    </ul>
-                                </div>
+                                  </div>
+                                  <div className='w-full h-0.5 bg-gray-300 rounded-lg mb-5' />
+                                  <div className='flex font-montserrat text-xl font-bold mb-5 text-2xl'>
+                                    <div className='pl-5 w-4/12'>Name</div>
+                                    <div className='w-2/12'>Price</div>
+                                    <div className='w-2/12'>Sold</div>
+                                    <div className='w-2/12'>Quota</div>
+                                    <div className='w-2/12'></div>
+                                  </div>
+                                  <div className='w-full h-0.5 bg-gray-300 rounded-lg mb-5' />
                                 <div>
                                     {ticketData!.name.map((name,index) => (
                                     <div key={name}>
-                                    <ul className="flex flex-row justify-between my-5 mx-20 text-base">   
-                                        <li>{name}</li>
-                                        <li></li>
-                                        <li>{ticketData!.price[index]}</li>
-                                        <li>{ticketData!.sold[index]}</li>
-                                        <li>{ticketData!.quota[index]}</li>
-                                        <button className='text-red-600 font-bold'>remove</button>
-                                    </ul>
+                                    <div className='flex font-montserrat text-lg mb-5 text-2xl'>
+                                        <div className='pl-5 w-4/12'>{name}</div>
+                                        <div className='w-2/12'>{ticketData!.price[index]}</div>
+                                        <div className='w-2/12'>{ticketData!.sold[index]}</div>
+                                        <div className='w-2/12'>{ticketData!.quota[index]}</div> 
+                                        <button className='text-red-600 font-bold w-2/12'>remove</button>
+                                    </div>
                                     </div>
                                     ))}
                                 </div>
-                            </div>
-                            <div className="flex flex-grow flex-col text-center my-5">
-                                <div className="font-bold text-4xl">EventBud</div>
-                                <div className="">all right reserved</div>
-                            </div>
+                          {/*Footer*/}
+                          <div className='justify-center bg-white p-4 mt-10'>
+                            <h1 className='text-center text-black font-montserrat font-bold text-2xl'>EventBud</h1>
+                            <h1 className='text-center text-black font-montserrat'>all right reserved</h1>
+                          </div>
                         </>
                         : <ContentCreateNewTicketTypes/>
                     } 
