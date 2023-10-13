2b7f67eca254d95c64ec3a904bee7d494d7ab082
kyohonda <praewa.ho@ku.th>
2023-10-13 13:59:30 +0700

    create url for each event type

---
+++ src/pages/eventdetail/OnePrice/[id].tsx
@@ -0,0 +1,12 @@
+import EventDetails from '../../eventdetail';
+
+const EventDetail = () => {
+
+  return (
+    <div>
+      <EventDetails></EventDetails>
+    </div>
+  );
+};
+
+export default EventDetail;
