fe895533912927dd96c39703932fb7afb0571b0c
qxntm <russarin.n@ku.th>
2023-10-19 16:49:21 +0700

    ticket for phone

--- src/components/ticket/ticketforphone.tsx
+++ src/components/ticket/ticketforphone.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React from "react";
 import SVG from 'react-inlinesvg';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { set } from "zod";
+import Link from "next/link";
 
 const ticketforphone = (props:any) => {
+    
+
     const eventTicket = props
     const qrcode = require('qrcode') 
-    
+    // console.log(eventTicket.firstname)
+    // console.log(eventTicket.ticketID)
+    // console.log(eventTicket.eventID)
+
+    // Convert payload(string) to SVG(qrcode)
     var getSVG = ''
     const payload = eventTicket.ticketID
     const options = { type: 'svg', color: { dark: '#000', light: '#fff' } }
@@ -48,9 +58,9 @@ const ticketforphone = (props:any) => {
         if(eventTicket.status=="available")
         return(
             <>
-            <button className="flex flex-row justify-center" onClick={setDataToLocalStorage}>
-                <p className="font-montserrat font-bold text-sm text-white w-3/4">Send this ticket to your friend</p>
-                <img className="w-auto h-6 mt-3" src="../images/tickets/sendIcon.png"></img>
+            <button className="flex flex-row justify-center gap-2" onClick={setDataToLocalStorage}>
+                <p className="font-montserrat font-bold text-xs text-white w-3/4">Send this ticket to your friend</p>
+                <img className="w-auto h-3 mt-3" src="../images/tickets/sendIcon.png"></img>
             </button>
             </>
         )
@@ -62,12 +72,12 @@ const ticketforphone = (props:any) => {
     const qr = () => {
         if(eventTicket.status=="available")
         return(
-            <SVG className="w-40" src= {getSVG}/>
+            <SVG className="" src= {getSVG}/>
         )
         else{
             return(
             <>
-                <SVG className="w-40 opacity-20 blur-sm" src= {getSVG}/>
+                <SVG className="opacity-20 blur-sm" src= {getSVG}/>
             </>
         )
         }
@@ -75,15 +85,16 @@ const ticketforphone = (props:any) => {
 
     let eventName_ch = eventTicket.eventName
     const checklength = () => {
-    console.log(eventTicket.eventName.length,eventTicket.eventName)
-    if(eventTicket.eventName.length > 75){
-        eventName_ch = eventTicket.eventName.substring(0,75) + "..."
+    console.log(eventTicket.eventName?.length,eventTicket.eventName)
+    if(eventTicket.eventName?.length > 60){
+        eventName_ch = eventTicket.eventName.substring(0,60) + "..."
     }
     }
     checklength()
 
     const data = {
         ticketID : eventTicket.ticketID,
+        userID: eventTicket.userID,
         firstname: eventTicket.firstname,
         lastname: eventTicket.lastname,
         eventName: eventName_ch,
@@ -100,12 +111,64 @@ const ticketforphone = (props:any) => {
     const setDataToLocalStorage = () => {
         localStorage.setItem('data', JSON.stringify(data));
     }
-  return (
-    <>
-        <div>
-
-        </div>
-    </>
-  )
+    
+    return(
+        <>
+            <div className="flex w-auto mx-3">
+                <div className="bg-black rounded-md w-1/3 px-3">
+                    <div className="flex flex-col justify-items-center my-8 gap-2 text-sm">
+                        <div className="font-montserrat font-bold text-white">no.</div>
+                        {/* <div className="text-white font-montserrat font-bold ml-2">no. {eventTicket.ticketID}</div> */}
+                        <img className="rounded-md w-auto" src={eventTicket.eventImage}></img>
+                            <Link href={`/sendticket`}>
+                            {button()}
+                            </Link>
+                        </div>
+                </div>
+                <div className="bg-[#F9F9F9] w-2/3 rounded-md px-4 py-2 shadow-lg">
+                    <div className="flex flex-col justify-between mt-4">
+                        <div className="flex flex-row-2 justify-between">
+                            <div className="font-montserrat font-bold text-sm text-[#D40000]">{parsedate}</div>
+                        </div>
+                        <div className="font-montserrat font-bold text-base text-black mt-2 w-auto">{eventName_ch}</div>
+                        <div className="flex flex-row-2 ">
+                            <div className="">
+                                <div className="font-montserrat font-medium text-sm text-black my-2 w-auto">{eventTicket.location}</div>
+                                <div className="flex gap-2 w-auto">
+                                    <div className="flex flex-row-2 w-1/2 justify-between h-auto">
+                                        <div className="">
+                                            <div className="font-montserrat font-bold text-sm text-black">ZONE</div>
+                                            <div className="font-montserrat font-bold text-base text-black">{zone}</div>
+                                            <div className="font-montserrat font-bold text-sm text-black">ROW</div>
+                                            <div className="font-montserrat font-bold text-base text-black">{row}</div>
+                                        </div>
+                                        <div className="">
+                                            <div className="font-montserrat font-bold text-sm text-black">GATE</div>
+                                            <div className="font-montserrat font-bold text-base text-black">{gate}</div>
+                                            <div className="font-montserrat font-bold text-sm text-black">SEAT</div>
+                                            <div className="font-montserrat font-bold text-base text-black">{seat}</div>
+                                        </div>
+                                    </div>
+                                    <div className="w-1/2">
+                                        {qr()}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="flex flex-row-2 justify-items-start gap-10 mt-3">
+                            <div className="">
+                                <div className="font-montserrat font-bold text-sm text-black">First Name</div>
+                                <div className="font-montserrat font-bold text-base text-black">{eventTicket.firstname}</div>
+                            </div>
+                            <div className="">
+                                <div className="font-montserrat font-bold text-sm text-black">Last Name</div>
+                                <div className="font-montserrat font-bold text-base text-black">{eventTicket.lastname}</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 export default ticketforphone;
